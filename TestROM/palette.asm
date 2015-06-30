c6b3d3da7e269016aa8a270b2e1f727d970f45cf
vahidheidari <crystal.delta@gmail.com>
2015-06-29 21:58:07 -0700

    License added.

--- TestROM/palette.asm
+++ TestROM/palette.asm
@@ -1,3 +1,27 @@
+;-----------------------------------
+; This is an 'NES' test program.
+; Assembler is DASM.exe
+; Date 2014/02/20 Thursday 21:49
+;
+; NES_DEV is a cross-platform, portable, and hand-held NES emulator.
+;
+; Copyright (C) 2015  Vahid Heidari (DeltaCode)
+; 
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;
+;-----------------------------------
+
 PALETTE_TABLE
 IMAGE_PALETTE
 	dc.b $0D
