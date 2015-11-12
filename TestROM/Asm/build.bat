5cff1782e94ad3e0232eacc28b5e8f7d919912e9
VahidHeidari <crystal.delta@gmail.com>
2015-11-11 17:45:04 -0800

    Build script cartridge header path corrected.

--- TestROM/Asm/build.bat
+++ TestROM/Asm/build.bat
@@ -3,8 +3,8 @@
 set DASM="C:\Documents and Settings\Delta\My Documents\ARM\DOCS\dasm32\dasm.exe"
 set RESOURCE="Resources"
 
-%DASM% intro-rom.asm -v2 -f3 -ointro-rom.bin && copy /b intro-rom.bin+%RESOURCE%\ascii.chr test.nes
+%DASM% intro-rom.asm -v0 -f3 -ointro-rom.bin && copy /b %RESOURCE%\Header+intro-rom.bin+%RESOURCE%\ascii.chr test.nes
 
-REM %RESOURCE%\Header+
+
 
 pause
